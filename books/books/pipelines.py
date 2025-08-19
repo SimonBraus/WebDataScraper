f411a7fa0c6c13df2d80e377bf256f078ade9f9b
Simon Braus <simon.braus@icloud.com>
2025-08-19 14:05:32 +0200

    connect btw spd and db

--- books/books/pipelines.py
+++ books/books/pipelines.py
@@ -8,6 +8,33 @@
 from itemadapter import ItemAdapter
 
 
-class BooksPipeline:
+class MongoPipeline:
+    #specifies the name of the MongoDB collection where you want to store the items. This should match the name of the collection that you set up earlier.
+    COLLECTION_NAME = "books"
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+    #initializes the pipeline with the MongoDB URI and database name. You can access this information because you’re fetching it from the Crawler using the .from_crawler() class method.
+    
+    @classmethod
+    #.from_crawler() is a class method that gives you access to all core Scrapy components, such as the settings. In this case, you use it to retrieve the MongoDB settings from settings.py through the Scrapy crawler.
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get("MONGO_URI"),
+            mongo_db=crawler.settings.get("MONGO_DATABASE"),
+        )
+
+    #opens a connection to MongoDB when the spider starts.
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    #closes the MongoDB connection when the spider finishes.
+    def close_spider(self, spider):
+        self.client.close()
+
+    #inserts each scraped item into the MongoDB collection. This method usually contains the core functionality of a pipeline.
     def process_item(self, item, spider):
+        self.db[self.COLLECTION_NAME].insert_one(ItemAdapter(item).asdict())
         return item
